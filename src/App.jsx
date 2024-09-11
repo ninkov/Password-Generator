f33be53055b06cc6214a57a4fb751498053ed1af
Ninkov <ninkov82@gmail.com>
2024-09-11 11:28:11 +0300

    All done UI part

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,68 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
   return (
-    
-      <div>
-       <h1>Password Generator</h1>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-600">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3 "
+          placeholder="Password"
+          readOnly
+        />
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          copy
+        </button>
       </div>
-      
-  
-  )
+      <div className="flex text-sm gap-x-2">
+        <div className="flex text-sm gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+            name=""
+            id=""
+          />
+          <label htmlFor="length">Length: {length}</label>
+        </div>
+        <div className="flex text-sm gap-x-1">
+          <input
+            type="checkbox"
+            defaultValue={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+            name=""
+            id=""
+          />
+          <label htmlFor="number">Numbers</label>
+        </div>
+        <div className="flex text-sm gap-x-1">
+          <input
+            type="checkbox"
+            defaultValue={charAllowed}
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+            name=""
+            id=""
+          />
+          <label htmlFor="charInput">Character</label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
